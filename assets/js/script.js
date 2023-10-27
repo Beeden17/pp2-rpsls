dfe427a1ae26bf48c93c48436e2503809ab495cf
Beeden17 <>
2023-10-27 23:05:53 +0000

    started js file added some function ideas

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+    //Allow user to use enter key to be READY!
+    document.getElementsByTagName("ready-button").addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        };
+
+//Functions to use for game:
+
+//
+function playerNameCharacter(){
+
+}
+
+function rungame(){
+
+}
+
+function checkWinner() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementRound() {
+
+}
+
+function resetPositive() {
+
+}
+
+function youWin(){
+
+}
+
+function youLose(){
+
+}
