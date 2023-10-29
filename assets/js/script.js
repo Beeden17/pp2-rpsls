79f21f0cfe775ee30a8ea9c0f83a223569b07cc9
Beeden17 <>
2023-10-29 18:55:04 +0000

    started event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,28 +13,44 @@ document.getElementsByTagName("ready-button").addEventListener("keydown", functi
  * declare variables for computer character
  * declare variables for weapon selection
  */
-let enterName = document.getElementById('entername');
+var enterName = document.getElementById('entername');
 let playerCharacter = document.getElementsByName('character-choice');
 let computerCharacter = document.getElementsByName('character-choice') !== playerCharacter;
 let playerWeapon = document.getElementsByClassName('weapons-div');
 let computerWeapon = document.getElementsByClassName('weapons-div') !== playerWeapon;
-let gameRound = document.getElementById('round-counter');
-let playerScore = document.getElementById('user-score');
+const start = document.getElementsByClassName('get-ready-button');
+var gameRound = document.getElementById('round-counter');
+var playerScore = document.getElementById('user-score');
 let computerScore = document.getElementById('computer-score');
 let matchWinner = document.getElementById('the-winner-is');
-let resetButton = document.getElementsByClassName('reset-button');
+const resetButton = document.getElementsByClassName('reset-button');
 
-//Functions to use for game
+// Add event listener to ready to  play button
+
+start.addEventListener("click", function () {
+    if (this.getAttribute('username'));
+    if (this.getAttribute('data-choice') === "submit");{
+        playerName;
+        playerIcon;
+        computerIcon;
+
+    }
+
+// The main game loop
+function runGame() {
+
+}
+
+// Function to add custom name to player zone
 function playerName(){
 
 }
-
-function runGame(){
+// Adds chosen character as icon
+function playerIcon(){
 
 }
-
-//Generate computer player random
-function computerChoice() {
+// Generate computer player random
+function computerIcon() {
         Math.floor(Math.random() * 5) + 1;
 }
 function checkWinner() {
